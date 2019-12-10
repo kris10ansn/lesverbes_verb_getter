62998ad7443577417fcce63fa12fe94e5b31aeeb
kris10ansn <telefon.kristian@gmail.com>
2019-12-10 21:40:12 +0100

    Added command argument support

--- js/index.js
+++ js/index.js
@@ -1,58 +1,83 @@
 const getConjugations = require("./getConjugations");
 const inquirer = require("inquirer");
 
+let arg3 = process.argv[3];
 async function prompt(verb) {
 	return new Promise(async (resolve, reject) => {
-		const conjugations = await getConjugations(verb)
-								.catch(err => {
-									reject(`Verb "${verb}" ${err}`)
-									return null;
-								});
-								
-		if(conjugations === null) return;
+		const conjugations = await getConjugations(verb).catch(err => {
+			reject(`Verb "${verb}" ${err}`);
+			return null;
+		});
 
-		inquirer
-			.prompt([
-				{
-					name: "form",
-					message: `Which form would you like to see ${verb} in?`,
-					choices: [...conjugations.map(c => c.title)],
-					type: "list"
+		if (conjugations === null) return;
+
+		const answers = arg3
+			? { form: arg3 }
+			: await inquirer.prompt([
+					{
+						name: "form",
+						message: `Which form would you like to see ${verb} in?`,
+						choices: [...conjugations.map(c => c.title)],
+						type: "list"
+					}
+			  ]);
+
+		if (arg3) {
+			conjugations.forEach(conjugation => {
+				// Remove accent and convert to lowercase, replace - with space
+				const simplify = str =>
+					str
+						.normalize("NFD")
+						.replace(/[\u0300-\u036f]/g, "")
+						.replace(/\-/g, " ")
+						.toLowerCase();
+
+				const arg = simplify(arg3);
+				const formSimple = simplify(conjugation.title);
+
+				if (arg === formSimple) {
+					answers.form = conjugation.title;
 				}
-			])
-			.then(answers => {
-				const form = answers["form"];
-				const conjugation = conjugations.find(c => c.title === form);
-
-				resolve(conjugation);
 			});
+
+			arg3 = null;
+		}
+
+		const form = answers["form"];
+		const conjugation = conjugations.find(c => c.title === form);
+
+		resolve(conjugation);
 	});
 }
 
-function start() {
-	inquirer
-		.prompt([
-			{
-				name: "verb",
-				message: "What verb are you looking for? ",
-				type: "input"
-			}
-		])
-		.then(async answers => {
-			const conjugations = await prompt(answers["verb"])
-				.catch(err => {
-					console.log(err);
-					return null;
-				});
+let verb = process.argv[2];
+async function start() {
+	const answers = verb
+		? { verb }
+		: await inquirer.prompt([
+				{
+					name: "verb",
+					message: "What verb are you looking for? ",
+					type: "input"
+				}
+		  ]);
 
-			if(conjugations !== null) {
-				console.log(`
+	verb = null;
+
+	const conjugations = await prompt(answers["verb"]).catch(err => {
+		console.log(err);
+		return null;
+	});
+
+	if (conjugations !== null) {
+		console.log(
+			`
 					${conjugations.title}:
 					${conjugations.content}
-				`.replace(/\t/g, ''));
-			}
+				`.replace(/\t/g, "")
+		);
+	}
 
-			setTimeout(start, 200);
-		});
+	setTimeout(start, 200);
 }
 start();
